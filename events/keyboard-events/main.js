40b70d4018b8692d2a2d2f4b004bcaf80785fa04
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-11-23 16:24:15 -0300

    Testing character and non character key events

--- events/keyboard-events/main.js
+++ events/keyboard-events/main.js
@@ -1,10 +1,26 @@
 // Select element
 let msg = document.querySelector("#message");
 
-
+// Character keys: keydown, keypress and keyup are fired sequentially
+// Non-character keys: keydown and keyup events are fired 
 // event properties
 msg.addEventListener("keydown", e => {
     console.log(
-        `key= ${e.key}, 
+        `Keydown:
+        \nkey= ${e.key}, 
+        \ncode= ${e.code}`)
+});
+
+msg.addEventListener("keypress", e => {
+    console.log(
+        `keypress (Only for character keys):
+        \nkey= ${e.key}, 
+        \ncode= ${e.code}`)
+});
+
+msg.addEventListener("keyup", e => {
+    console.log(
+        `Keyup:
+        \nkey= ${e.key}, 
         \ncode= ${e.code}`)
 });
