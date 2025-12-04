044359abf0c82bc1332754b193a88f5488431c5e
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-12-04 11:06:07 -0300

    Map to objects complete

--- array-methods/main.js
+++ array-methods/main.js
@@ -11,9 +11,28 @@ console.log("___________________________")
 
 // 1. Map to names
 let names = users.map(user => user.name); // This was easy
-console.log("Map to names:")
+console.log("1. Map to names:")
 console.log("Array of strings")
 console.log(names); 
 console.log("___________________________")
 
+// 2. Map to object
+//Reassign users
+john = { name: "John", surname: "Smith", id: 1 };
+pete = { name: "Pete", surname: "Hunt", id: 2 };
+mary = { name: "Mary", surname: "Key", id: 3 };
 
+users = [john, pete, mary];
+
+console.log("2. Map to objects")
+console.log(users)
+
+let usersMapped = users.map(e => ({
+    fullName: `${e.name} ${e.surname}`,
+    id: `${e.id}`,
+}));
+
+// Forgot parentheses to create new obj property
+console.log("New objects with fullName:")
+console.log(usersMapped)
+console.log("_______________________")
