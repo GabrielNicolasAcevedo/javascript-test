06093dbe8cb83b8d29c4e9fd7b200f149e5fa1a6
Gabriel Acevedo <106357723+GabrielNicolasAcevedo@users.noreply.github.com>
2025-12-04 11:57:57 -0300

    Sort users by age complete

--- array-methods/main.js
+++ array-methods/main.js
@@ -5,16 +5,16 @@ let mary = {name: "Mary", age: 28};
 
 let users = [john, pete, mary];
 
-console.log("Array of objects:")
-console.log(users)
-console.log("___________________________")
+console.log("Array of objects:");
+console.log(users);
+console.log("___________________________");
 
 // 1. Map to names
 let names = users.map(user => user.name); // This was easy
-console.log("1. Map to names:")
-console.log("Array of strings")
+console.log("1. Map to names:");
+console.log("Array of strings");
 console.log(names); 
-console.log("___________________________")
+console.log("___________________________");
 
 // 2. Map to object
 //Reassign users
@@ -24,8 +24,8 @@ mary = { name: "Mary", surname: "Key", id: 3 };
 
 users = [john, pete, mary];
 
-console.log("2. Map to objects")
-console.log(users)
+console.log("2. Map to objects");
+console.log(users);
 
 let usersMapped = users.map(e => ({
     fullName: `${e.name} ${e.surname}`,
@@ -33,6 +33,28 @@ let usersMapped = users.map(e => ({
 }));
 
 // Forgot parentheses to create new obj property
-console.log("New objects with fullName:")
-console.log(usersMapped)
-console.log("_______________________")
+console.log("New objects with fullName:");
+console.log(usersMapped);
+console.log("_______________________");
+
+// 3. Sort Users by Age
+//Reassign users
+john = { name: "John", age: 25 };
+pete = { name: "Pete", age: 30 };
+mary = { name: "Mary", age: 28 };
+
+let arr = [ pete, john, mary ];
+
+console.log("3. Sort Users by Age");
+console.log(arr);
+
+function sortByAge(array) {
+    return array.sort((a,b) => {
+        return a.age - b.age
+    })
+}; 
+// More difficult, saw a youtube lesson on sorting to refresh memory
+sortByAge(arr);
+console.log(arr);
+console.log("___________________________")
+
